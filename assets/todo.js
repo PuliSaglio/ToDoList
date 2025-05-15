cf4cf52e687e6c760fe882337a2b92853bae38f8
pulisaglio <pulisaglio@gmail.com>
2025-05-15 11:28:55 -0300

    Refatoração de todo.js para seguir boas praticas

--- assets/todo.js
+++ assets/todo.js
@@ -3,58 +3,98 @@ document.addEventListener('DOMContentLoaded' , function(){
 
     form.addEventListener('submit', function(event){
         event.preventDefault();
-        adicionarTarefa();
+        const cardTarefa = criarCardTarefa(coletarDadosForm());
+        estilizarCardTarefa(cardTarefa);
+        adicionarTarefa(cardTarefa);
+
         modal.classList.add('d-none');
     });
 });
 
+const colunaTodo = document.getElementById('colunaAfazer');
+
 const btnAbrirModal = document.getElementById('btnAbrirModal');
 const btnFecharModal = document.getElementById('btnFecharModal');
+
 const modal = document.getElementById('modalTarefa');
+
 
 btnAbrirModal.onclick = () => modal.classList.remove('d-none');
 btnFecharModal.onclick = () => modal.classList.add('d-none');
 
+function coletarDadosForm(){
+    const tituloTarefa = document.getElementById('tituloTarefa').value;
+    const descricaoTarefa = document.getElementById('descricaoTarefa').value;
+    const dataTarefa = document.getElementById('dataTarefa').value;
+    const prioridadeTarefa = document.getElementById('prioridadeTarefa').value;
+    const notificacaoTarefa = document.querySelector('input[name="notificacao"]:checked').value;
 
-function adicionarTarefa(){
+    const tarefa = {
+        titulo: tituloTarefa,
+        descricao: descricaoTarefa,
+        data: dataTarefa,
+        prioridade: prioridadeTarefa,
+        notificacao: notificacaoTarefa,
+        dataCriacao: new Date()
+    };
 
-    const colunaTodo = document.getElementById('colunaAfazer');
+    return tarefa;
+}
+
+function criarCardTarefa(tarefa){
     const cardSombreado = document.createElement('div');
-    cardSombreado.className = "card shadow-sm mb-3";
     const novaTarefa = document.createElement('div');
-    novaTarefa.className = "card-body";
     const cabecalhoCard = document.createElement('div');
-    cabecalhoCard.className = "d-flex justify-content-between align-items-center mb-2";
     const badge = document.createElement('span');
-    badge.className = "badge bg-primary";
 
     const tituloTarefa = document.createElement('h5');
-    tituloTarefa.innerHTML = document.getElementById('tituloTarefa').value;
-    tituloTarefa.className = "card-title mb-0";
+    tituloTarefa.innerHTML = tarefa.titulo;
+
     const descricaoTarefa = document.createElement('p');
-    descricaoTarefa.className = "card-text ms-2 mb-2";
-    descricaoTarefa.innerHTML = document.getElementById('descricaoTarefa').value;
+    descricaoTarefa.innerHTML = tarefa.descricao;
+
     const dataTarefa = document.createElement('time');
-    dataTarefa.dateTime = document.getElementById('dataTarefa').value;
-    dataTarefa.innerHTML = document.getElementById('dataTarefa').value;
+    dataTarefa.innerHTML = tarefa.data;
 
     const prioridadeTarefa = document.createElement('p');
-    prioridadeTarefa.innerHTML = document.getElementById('prioridadeTarefa').value;
-    prioridadeTarefa.className = "fw-bold text-danger mb-0";
+    prioridadeTarefa.innerHTML = tarefa.prioridade;
+
     const notificacaoTarefa = document.createElement('p');
-    notificacaoTarefa.innerHTML = document.querySelector('input[name="notificacao"]:checked').value;
+    notificacaoTarefa.innerHTML = tarefa.notificacao;
 
-    const dataCriacao = new Date();
+    const cardTarefa = {
+        containerSombreado: cardSombreado,
+        containerTarefa: novaTarefa,
+        cabecalho: cabecalhoCard,
+        badge: badge,
+        titulo: tituloTarefa,
+        descricao: descricaoTarefa,
+        data: dataTarefa,
+        prioridade: prioridadeTarefa,
+        notificacao: notificacaoTarefa
+    };
 
-    console.log(dataCriacao);
-    badge.appendChild(dataTarefa)
-    novaTarefa.appendChild(cabecalhoCard);
-    cabecalhoCard.appendChild(tituloTarefa);
-    cabecalhoCard.appendChild(badge);
-    novaTarefa.appendChild(descricaoTarefa);
-    novaTarefa.appendChild(prioridadeTarefa);
-
-    colunaTodo.appendChild(cardSombreado);
-    cardSombreado.appendChild(novaTarefa);
+    return cardTarefa;
     
+}
+
+function estilizarCardTarefa(cardTarefa){
+    cardTarefa.containerSombreado.className = "card shadow-sm mb-3";
+    cardTarefa.containerTarefa.className = "card-body";
+    cardTarefa.cabecalho.className = "d-flex justify-content-between align-items-center mb-2";
+    cardTarefa.badge.className = "badge bg-primary";
+    cardTarefa.titulo.className = "card-title mb-0";
+    cardTarefa.descricao.className = "card-text ms-2 mb-2";
+    cardTarefa.prioridade.className = "fw-bold text-danger mb-0"
+}
+
+function adicionarTarefa(cardTarefa){
+    cardTarefa.badge.appendChild(cardTarefa.data)
+    cardTarefa.containerTarefa.appendChild(cardTarefa.cabecalho);
+    cardTarefa.cabecalho.appendChild(cardTarefa.titulo);
+    cardTarefa.cabecalho.appendChild(cardTarefa.badge);
+    cardTarefa.containerTarefa.appendChild(cardTarefa.descricao);
+    cardTarefa.containerTarefa.appendChild(cardTarefa.prioridade);
+    colunaTodo.appendChild(cardTarefa.containerSombreado);
+    cardTarefa.containerSombreado.appendChild(cardTarefa.containerTarefa);
 }
